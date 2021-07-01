5ca207a8ee39d4c1f5c2b3389dc7e3573d0b3f2f
Fluffy-Lion <leoncooper31@gmail.com>
2021-07-01 11:37:17 +0100

    first commit, set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs"
 
-function App() {
+const App = () => {
+  const buttons = useState(["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="])
+  const [total, setTotal] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>calculator v3</h1>
     </div>
   );
 }
