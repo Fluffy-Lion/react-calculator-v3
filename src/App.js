fa8dbffedb18d1d2f5c9d2a175fd73b2044c37b6
Fluffy-Lion <leoncooper31@gmail.com>
2021-07-01 11:57:24 +0100

    added evaluate and button component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import { useState } from "react";
-import { evaluate } from "mathjs"
+import { evaluate } from 'mathjs'
 
 const App = () => {
-  const buttons = useState(["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="])
+  const buttons = ["7", "8", "9", "c", "4", "5", "6", "*", "1", "2", "3", "/", "+", "0", "-", "="]
   const [total, setTotal] = useState("")
+
+  const clickHandler = (value) => {
+    if(value === "="){
+      setTotal(evaluate(total))
+    } else if (value === "c"){
+      setTotal("")
+    } else {
+      setTotal( total + value)
+    }
+  }
   return (
     <div>
       <h1>calculator v3</h1>
+      <h2>{total}</h2>
+      {buttons.map((button, index) => {
+        return (
+          <div>
+          <Button key={index} value={button} clickHandler={clickHandler} />
+          </div>
+        )
+      })}
     </div>
   );
+}
+
+const Button = ({ clickHandler, value }) => {
+  return (
+    <button onClick={() => clickHandler(value)}>
+      {value}
+    </button>
+  )
 }
 
 export default App;
